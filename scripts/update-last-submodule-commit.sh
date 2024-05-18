ea60ef03ac8c679bbfb565d3db3e008390a0a07f
Jean Claveau <1556489+jclaveau@users.noreply.github.com>
2024-05-19 01:24:46 +0200

    disabling commit scrip

--- scripts/update-last-submodule-commit.sh
+++ scripts/update-last-submodule-commit.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+# SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-cd "$SCRIPT_DIR/.."
-pwd
-git add index.js
-git add dist/index.js
-git commit --amend --no-edit
-git push -f
-cd "$SCRIPT_DIR/../.."
-pwd
-git add railway-pr-deploy
-git commit --amend --no-edit
-git push -f
+# echo "Committing the submodule"
+# cd "$SCRIPT_DIR/.."
+# # pwd
+# # git add index.js
+# # git add dist/index.js
+# git commit --amend --no-edit
+# git push -f
+
+# echo "Committing the root repo"
+# cd "$SCRIPT_DIR/../.."
+# pwd
+# git add railway-pr-deploy
+# git commit --amend --no-edit
+# git push -f
